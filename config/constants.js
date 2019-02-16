8fcb630036501211074a220d484b00d3bb35513d
Mihailo Stefanovic <mihailo@shaw.ca>
2019-02-15 23:17:06 -0800

    Adding RSA and Argon2 constants

--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,5 @@
+const argon2 = require('argon2')
+
 module.exports = Object.freeze({
   TAGS: 'TAGS',
   MALLS: 'MALLS',
@@ -7,6 +9,15 @@ module.exports = Object.freeze({
   DEALS: 'DEALS',
   USER_AUTH: 'USER_AUTH',
   STORE_AUTH: 'STORE_AUTH',
+  PRIVATE_KEY_PATH: './config/keys/private_key.pem',
+  PUBLIC_KEY_PATH: './config/keys/public_key.pem',
+  ARGON2_PROPERTIES:
+  {
+    type: argon2.argon2d,
+    memoryCost: 2048,     // 2MB per thread
+    parallelism: 8,       // 8 threads (32MB RAM per hash)
+    hashLength: 50,
+  },
   ERR:
   {
     "Error" : "An error has occured"
