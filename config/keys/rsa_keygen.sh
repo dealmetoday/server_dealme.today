fbc79d9cf4647b56fca4fc8c118909b4bd9c220b
Mihailo Stefanovic <mihailo@shaw.ca>
2019-02-15 21:22:49 -0800

    Fixed keygen script bug
    Was creating RSA key pair in directory where script was run, not in the
    config/keys directory as expected

--- config/keys/rsa_keygen.sh
+++ config/keys/rsa_keygen.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 KEYPATH="$( cd "$(dirname "$0")"; pwd -P )"
-if [[ ! -f $KEYPATH/private_key.pem || ! -f $KEYPATH/public_key.pem ]]
+PRVKEY=$KEYPATH/private_key.pem
+PUBKEY=$KEYPATH/public_key.pem
+
+if [[ ! -f $PRVKEY || ! -f $PUBKEY ]]
 then
-    openssl genpkey -algorithm RSA -out private_key.pem -pkeyopt rsa_keygen_bits:4096
-    openssl rsa -pubout -in private_key.pem -out public_key.pem
+    openssl genpkey -algorithm RSA -out $PRVKEY -pkeyopt rsa_keygen_bits:4096
+    openssl rsa -pubout -in $PRVKEY -out $PUBKEY
 else
     echo "Public and private keys already exist"
     echo "Will not generate another key pair"
