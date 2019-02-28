e7b59d4339cded66a3455e1b884437465f638965
Ryan Liu <dio.ryanliu@hotmail.com>
2019-02-28 00:33:59 -0800

    Adding a few things for test. Confirm that requests work and will be changing

--- serverless/dealme.today/src/app.js
+++ serverless/dealme.today/src/app.js
@@ -13,4 +13,13 @@ app.use(helmet());
 require('./db');
 app.use(routes);
 
+app.get('/', function(req, res) {
+  var body = "You have hit the root of the server - which currently doesn't do anything.";
+  res.set('Content-Type', 'text/plain');
+  res.set('charset', 'utf-8');
+  res.charset =
+  res.writeHead(200);
+  res.end(body);
+});
+
 module.exports = app;
